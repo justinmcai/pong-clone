536414126ff9722dc370c361aa93270e9eb81d49
justinmcai <jc9744@nyu.edu>
2024-06-29 22:05:47 -0400

    Single player paddle gets stuck. Endgame msg looks weird. Haven't implemented ball 2&3 logic

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Justin Cai
+* Assignment: Pong Clone
+* Date due: 2024-06-29, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
@@ -19,12 +29,14 @@
 #include <ctime>
 
 enum AppStatus { RUNNING, TERMINATED };
-enum GameMode { SINGLE, MULTI};
+enum GameMode { SINGLE, MULTI };
+enum GameStatus { START, WINNER1, WINNER2 };
+enum NumBalls { ONE, TWO, THREE };
 
 constexpr float WINDOW_SIZE_MULT = 1.0f;
 
-constexpr int WINDOW_WIDTH = 800 * WINDOW_SIZE_MULT,
-WINDOW_HEIGHT = 600 * WINDOW_SIZE_MULT;
+constexpr int WINDOW_WIDTH = 1200 * WINDOW_SIZE_MULT,
+WINDOW_HEIGHT = 900 * WINDOW_SIZE_MULT;
 
 constexpr float BG_RED = 0.0f,
 BG_GREEN = 0.0f,
@@ -42,29 +54,36 @@ F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 
 // source: https://yorukura-anime.com/
-constexpr char KANO_SPRITE_FILEPATH[] = "paddle.png",
-MAHIRU_SPRITE_FILEPATH[] = "paddle.png",
-BALL_SPRITE_FILEPATH[] = "ball.png";
+constexpr char KANO_SPRITE_FILEPATH[] = "nagi.png",
+MAHIRU_SPRITE_FILEPATH[] = "isagi.png",
+BALL_SPRITE_FILEPATH[] = "soccer.png",
+P1_TEXT_FILEPATH[] = "p1win.png",
+P2_TEXT_FILEPATH[] = "p2win.png";
 
 constexpr float MINIMUM_COLLISION_DISTANCE = 1.0f;
-constexpr glm::vec3 INIT_SCALE = glm::vec3(0.2f, 1.0f, 0.0f),
-INIT_BALL_SCALE = glm::vec3(0.2f, 0.2f, 0.0f),
-INIT_POS_KANO = glm::vec3(4.9f, 0.0f, 0.0f),
-INIT_POS_MAHIRU = glm::vec3(-4.9f, 0.0f, 0.0f),
+constexpr glm::vec3 INIT_SCALE = glm::vec3(1.5f, 3.0f, 0.0f),
+INIT_BALL_SCALE = glm::vec3(1.0f, 1.0f, 0.0f),
+INIT_TEXT_SCALE = glm::vec3(5.0f, 5.0f, 0.0f),
+INIT_POS_KANO = glm::vec3(4.5f, 0.0f, 0.0f),
+INIT_POS_MAHIRU = glm::vec3(-4.5f, 0.0f, 0.0f),
 INIT_POS_BALL = glm::vec3(0.0f, 0.0f, 0.0f);
 
 SDL_Window* g_display_window;
 
 AppStatus g_app_status = RUNNING;
 GameMode g_game_mode = MULTI;
+GameStatus g_game_status = START;
+NumBalls g_num_balls = ONE;
 ShaderProgram g_shader_program = ShaderProgram();
-glm::mat4 g_view_matrix, g_kano_matrix, g_projection_matrix, g_trans_matrix, g_mahiru_matrix, g_ball_matrix;
+glm::mat4 g_view_matrix, g_kano_matrix, g_projection_matrix, g_trans_matrix, g_mahiru_matrix, g_ball_matrix, g_ball2_matrix, g_ball3_matrix, g_p1_matrix, g_p2_matrix;
 
 float g_previous_ticks = 0.0f;
 
 GLuint g_kano_texture_id;
 GLuint g_mahiro_texture_id;
 GLuint g_ball_texture_id;
+GLuint g_p1_texture_id;
+GLuint g_p2_texture_id;
 
 glm::vec3 g_kano_position = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 g_kano_movement = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -74,6 +93,12 @@ glm::vec3 g_mahiru_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
 glm::vec3 g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 g_ball_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_ball2_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_ball3_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball3_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
 //paddle speed
 float g_kano_speed = 5.0f;  // move 1 unit per second
@@ -154,8 +179,11 @@ void initialise()
 
     g_kano_matrix = glm::mat4(1.0f);
     g_mahiru_matrix = glm::mat4(1.0f);
-    //here = do I need to translate ball matrix?
     g_ball_matrix = glm::mat4(1.0f);
+    g_ball2_matrix = glm::mat4(1.0f);
+    g_ball3_matrix = glm::mat4(1.0f);
+    g_p1_matrix = glm::mat4(1.0f);
+    g_p2_matrix = glm::mat4(1.0f);
     g_mahiru_matrix = glm::translate(g_mahiru_matrix, glm::vec3(1.0f, 1.0f, 0.0f));
     g_mahiru_position += g_mahiru_movement;
 
@@ -172,6 +200,8 @@ void initialise()
     g_kano_texture_id = load_texture(KANO_SPRITE_FILEPATH);
     g_mahiro_texture_id = load_texture(MAHIRU_SPRITE_FILEPATH);
     g_ball_texture_id = load_texture(BALL_SPRITE_FILEPATH);
+    g_p1_texture_id = load_texture(P1_TEXT_FILEPATH);
+    g_p2_texture_id = load_texture(P2_TEXT_FILEPATH);
 
     // enable blending
     glEnable(GL_BLEND);
@@ -201,6 +231,21 @@ void process_input()
             case SDLK_t:
                 // Move the player right
                 g_game_mode = SINGLE;
+                break;
+
+            case SDLK_1:
+                // Move the player right
+                g_num_balls = ONE;
+                break;
+
+            case SDLK_2:
+                // Move the player right
+                g_num_balls = TWO;
+                break;
+
+            case SDLK_3:
+                // Move the player right
+                g_num_balls = THREE;
                 break;
 
             case SDLK_q:
@@ -268,27 +313,34 @@ void update()
     g_previous_ticks = ticks;
     float sign = 1.0f;
 
+    //-------------------------------------------------------BALL-------------------------------------------------------
+    //start ball
     if (g_ball_movement == glm::vec3(0.0f, 0.0f, 0.0f)) {
-        //g_ball_movement = glm::vec3(-1.0f, 1.0f, 0.0f);
+        g_ball_movement = glm::vec3(-1.0f, 1.0f, 0.0f);
     }
-
+    //bounce ball top
     if (INIT_POS_BALL.y + g_ball_position.y >= 3.7f) {
         g_ball_movement.y = -1.0f;
     }
+    //bounce ball bottom
     if (INIT_POS_BALL.y + g_ball_position.y <= -3.7f) {
         g_ball_movement.y = 1.0f;
     }
+    //end game right side lose
     if (INIT_POS_BALL.x + g_ball_position.x >= 5.0f) {
+        //g_ball_movement.x = -1.0f;
         g_ball_movement.x = 0.0f;
         g_ball_movement.y = 0.0f;
-        g_app_status = TERMINATED;
+        g_game_status = WINNER1;
     }
+    //end game left side lose
     if (INIT_POS_BALL.x + g_ball_position.x <= -5.0f) {
+        //g_ball_movement.x = 1.0f;
         g_ball_movement.x = 0.0f;
         g_ball_movement.y = 0.0f;
-        g_app_status = TERMINATED;
+        g_game_status = WINNER2;
     }
-    //left paddle
+    //right paddle and ball
     float x_distance_kano = fabs(g_kano_position.x + INIT_POS_KANO.x - g_ball_position.x) -
         ((INIT_SCALE.x + INIT_BALL_SCALE.x) / 2.0f);
 
@@ -299,7 +351,7 @@ void update()
     {
         g_ball_movement.x = -1.0f;
     }
-    //right paddle
+    //left paddle and ball
     float x_distance_mahiru = fabs(g_mahiru_position.x + INIT_POS_MAHIRU.x - g_ball_position.x) -
         ((INIT_SCALE.x + INIT_BALL_SCALE.x) / 2.0f);
 
@@ -310,45 +362,43 @@ void update()
     {
         g_ball_movement.x = 1.0f;
     }
-
-    //move ball
+    //move ball :: add direction * units per second * elapsed time
     g_ball_position += g_ball_movement * g_ball_speed * delta_time;
-    //LOG
-    //std::cout << "x = " << INIT_POS_BALL.x + g_ball_position.x << ": y = " << INIT_POS_BALL.y + g_ball_position.y << std::endl;
+        //LOG
+    std::cout << "x = " << INIT_POS_BALL.x + g_ball_position.x << ": y = " << INIT_POS_BALL.y + g_ball_position.y << std::endl;
 
-    //collision detection: paddle-border
-    if (INIT_POS_KANO.y + g_kano_position.y <= 3.3f && INIT_POS_KANO.y + g_kano_position.y >= -3.3f)
+    //------------------------------------------------------PADDLE------------------------------------------------------
+    //right paddle and border
+    if (INIT_POS_KANO.y + g_kano_position.y <= 3.0f && INIT_POS_KANO.y + g_kano_position.y >= -3.0f)
     {
         if (g_game_mode == SINGLE) {
-            g_kano_movement.y = -1.0f;
+            g_kano_movement.y = 1.0f;
         }
-        //movement :: add direction * units per second * elapsed time
         g_kano_position += sign * g_kano_movement * g_kano_speed * delta_time;
-        std::cout << "y = " << INIT_POS_KANO.y + g_kano_position.y << std::endl;
+        //std::cout << "y = " << INIT_POS_KANO.y + g_kano_position.y << std::endl;
     }
-    else if (INIT_POS_KANO.y + g_kano_position.y >= 3.3f){
-        //movement :: add direction * units per second * elapsed time
-        g_kano_position.y = 3.29999999999f;
+    else if (INIT_POS_KANO.y + g_kano_position.y >= 3.0f){
+        g_kano_position.y = 2.99999999999f;
         if (g_game_mode == SINGLE) {
-            sign = -1.0f;
+            sign *= -1.0f;
         }
     }
     else {
-        g_kano_position.y = -3.29999999999f;
+        g_kano_position.y = -2.99999999999f;
         if (g_game_mode == SINGLE) {
-            sign = 1.0f;
+            sign *= -1.0f;
         }
     }
-    //mahiru paddle
-    if (INIT_POS_MAHIRU.y + g_mahiru_position.y <= 3.3f && INIT_POS_MAHIRU.y + g_mahiru_position.y >= -3.3f)
+    //left paddle and border
+    if (INIT_POS_MAHIRU.y + g_mahiru_position.y <= 3.0f && INIT_POS_MAHIRU.y + g_mahiru_position.y >= -3.0f)
     {
         g_mahiru_position += g_mahiru_movement * g_kano_speed * delta_time;
     }
-    else if (INIT_POS_MAHIRU.y + g_mahiru_position.y >= 3.3f) {
-        g_mahiru_position.y = 3.29999999999f;
+    else if (INIT_POS_MAHIRU.y + g_mahiru_position.y >= 3.0f) {
+        g_mahiru_position.y = 2.9999999999f;
     }
     else {
-        g_mahiru_position.y = -3.29999999999f;
+        g_mahiru_position.y = -2.99999999999f;
     }
 
     //Regular update stuff
@@ -367,11 +417,14 @@ void update()
     g_kano_matrix = glm::scale(g_kano_matrix, INIT_SCALE);
     g_mahiru_matrix = glm::scale(g_mahiru_matrix, INIT_SCALE);
     g_ball_matrix = glm::scale(g_ball_matrix, INIT_BALL_SCALE);
-
-
-
-
-
+    if (g_game_status == WINNER1) {
+        g_p1_matrix = glm::mat4(1.0f);
+        g_p1_matrix = glm::scale(g_p1_matrix, INIT_TEXT_SCALE);
+    }
+    if (g_game_status == WINNER2) {
+        g_p2_matrix = glm::mat4(1.0f);
+        g_p2_matrix = glm::scale(g_p2_matrix, INIT_TEXT_SCALE);
+    }
 
 }
 
@@ -407,6 +460,19 @@ void render() {
     draw_object(g_kano_matrix, g_kano_texture_id);
     draw_object(g_mahiru_matrix, g_mahiro_texture_id);
     draw_object(g_ball_matrix, g_ball_texture_id);
+    if (g_num_balls == TWO) {
+        draw_object(g_ball2_matrix, g_ball_texture_id);
+    }
+    if (g_num_balls == THREE) {
+        draw_object(g_ball2_matrix, g_ball_texture_id);
+        draw_object(g_ball3_matrix, g_ball_texture_id);
+    }
+    if (g_game_status == WINNER1) {
+        draw_object(g_p1_matrix, g_p1_texture_id);
+    }
+    if (g_game_status == WINNER2) {
+        draw_object(g_p2_matrix, g_p2_texture_id);
+    }
 
 
     // We disable two attribute arrays now
